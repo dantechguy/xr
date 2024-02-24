af69c4b62ddfa4207b0bc9efe46d8d36e6c50a17
Qiaozhi <ql316@cam.ac.uk>
2024-02-24 15:28:53 +0000

    outline selected object

--- Assets/_Scripts/Spawn/ARSpawnedSelectable.cs
+++ Assets/_Scripts/Spawn/ARSpawnedSelectable.cs
@@ -10,7 +10,15 @@ public class ARSpawnedSelectable : MonoBehaviour
     [SerializeField] private GameObject selectionIndicator;
     [SerializeField] private SelectionInfo selectionInfo;
 
+    private Outline outline_;
     private bool selected_;
+
+    private void Awake()
+    {
+        outline_ = GetComponent<Outline>();
+        if (outline_ == null)
+            XLogger.LogError(Category.Select, "Outline not found");
+    }
 
     private void Start()
     {
@@ -21,12 +29,14 @@ public class ARSpawnedSelectable : MonoBehaviour
             XLogger.LogError(Category.Select, "Selection indicator not set");
 
         selectionIndicator.SetActive(selected_);
+        ToggleOutline(selected_);
     }
 
     public void OnSelect()
     {
         selected_ = true;
         selectionIndicator.SetActive(true);
+        ToggleOutline(true);
         selectionInfo.SetSelected(this);
     }
 
@@ -34,5 +44,11 @@ public class ARSpawnedSelectable : MonoBehaviour
     {
         selected_ = false;
         selectionIndicator.SetActive(false);
+        ToggleOutline(false);
+    }
+
+    public void ToggleOutline(bool _active)
+    {
+        outline_.enabled = _active;
     }
 }
